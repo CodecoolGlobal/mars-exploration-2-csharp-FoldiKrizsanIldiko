4869bfead7c04f30b42451928d2bbe5ce35ae106
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2023-07-24 16:03:26 +0200

    [test]<TestmapLoader>(start)

---
+++ TestMapExplorer/MapLoaderTest.cs
@@ -0,0 +1,31 @@
+using Codecool.MarsExploration.MapExplorer.MapLoader;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace TestMapExplorer;
+
+public class MapLoaderTest
+{
+    [Test]
+   public void TestCaracterNumbers()
+   {
+       string[,] map = new string[,]
+       {
+           { "1", "1", "1" },
+           { "1", "1", "1" },
+           { "1", "1", "1" }
+       };
+       Map testMap = new Map(map, true);
+       
+      //arrange
+      IMapLoader mapLoader = new MapLoader();
+      
+      //act
+     
+       Map result = mapLoader.Load("C:\\Users\\fkild\\Codecool\\OOP_module\\4_TW\\mars-exploration-2-csharp-FoldiKrizsanIldiko\\TestMapExplorer\\FakeMap.map");
+
+      //assert
+      //egyforma-e a méretük
+ 
+      Assert.That(testMap,Is.EqualTo(result) );
+   }
+}
