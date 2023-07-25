049a5bbf5118c1a183b3477a6a1a27437693dfa6
rekamakadi <r.makadi@gmail.com>
2023-07-25 15:01:51 +0200

    [test]<ConfigValidatorTest>(start)

--- TestMapExplorer/ConfigValidatorTest.cs
+++ TestMapExplorer/ConfigValidatorTest.cs
@@ -2,6 +2,12 @@
 [TestFixture]
 public class ConfigValidatorTest
 {
-   [Test]
-   
+   //[Test]
+   // IsConfigValid
+   // fakemap módosítani: nagyobb, üres helyekkel
+   // jó adatokkal
+   // hibás térképpel
+   // hibás filepath-al
+   // hibás timeouttal
+   // hibás recourse specified-dal
 }
