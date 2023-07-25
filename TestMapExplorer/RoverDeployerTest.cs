b2bbc88c1e0d944c89b0fb999eb23b53fd9c012a
rekamakadi <r.makadi@gmail.com>
2023-07-25 15:02:23 +0200

    [test]<RoverDeployerTest>(start)

---
+++ TestMapExplorer/RoverDeployerTest.cs
@@ -0,0 +1,7 @@
+﻿namespace TestMapExplorer;
+
+public class RoverDeployerTest
+{
+    //Ismert helyre téve assertben megadjuk, hogy a || b || c lesz-e - helyes coordináták közül kap egyet.
+    //
+}
