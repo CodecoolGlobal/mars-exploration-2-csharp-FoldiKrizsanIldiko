f48235c98a526f80e129373a8bf39376406b3a7f
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2023-07-25 19:31:54 +0200

    [test]<MapLoaderTest>(rcc) Map equality added, added bigger testMap

--- TestMapExplorer/MapLoaderTest.cs
+++ TestMapExplorer/MapLoaderTest.cs
@@ -11,9 +11,15 @@ public class MapLoaderTest
         //arrange
         string[,] map = new string[,]
         {
-            { "1", "1", "1" },
-            { "1", "1", "1" },
-            { "1", "1", "1" }
+            { "1"," "," "," ", "1"," "," "," ", "1" },
+            {" "," "," "," "," "," "," "," "," ",},
+            {" "," "," "," "," "," "," "," "," ",},
+            {" "," "," "," "," "," "," "," "," ",},
+            { "1"," "," "," ", "1"," "," "," ", "1" },
+            {" "," "," "," "," "," "," "," "," ",},
+            {" "," "," "," "," "," "," "," "," ",},
+            {" "," "," "," "," "," "," "," "," ",},
+            { "1"," "," "," ", "1"," "," "," ", "1" },
         };
         Map testMap = new Map(map, true);
     //    Map testMap1 = new Map(map, true);
@@ -24,6 +30,10 @@ public class MapLoaderTest
         Map result = mapLoader.Load($"{workDir}\\FakeMap.map");
         //assert
       //  Assert.That(testMap, Is.EqualTo(result));
+     
+      var comparer = new Map.MapEqualityComparer();
+      
+       Assert.That(comparer.Equals(testMap, result), Is.True);
         Assert.That(result.Representation, Is.EqualTo(testMap.Representation));
         Assert.That(result.SuccessfullyGenerated, Is.EqualTo(testMap.SuccessfullyGenerated));
     }
