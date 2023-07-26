5e6c88d28ae569a6de003966d771fa409430f0c1
Foldi Krizsan Ildiko <fkildiko13@gmail.com>
2023-07-26 15:48:18 +0200

    [feat]<FindEmptyAdjacentFields.cs>(ok) separated to class

---
+++ Codecool.MarsExploration.MapExplorer/Exploration/FindEmptyAdjacentFields.cs
@@ -0,0 +1,16 @@
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Service;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Exploration;
+
+public static class FindEmptyAdjacentField
+{
+    public static IEnumerable<Coordinate> FindEmptyAdjacentFields(ICoordinateCalculator coordinateCalculator, Map map, Coordinate currentPosition, Coordinate landingSpot)
+    {
+        var toReturn = coordinateCalculator.GetAdjacentCoordinates(currentPosition, map.Representation.GetLength(0))
+                .Where(c => map.Representation[c.X, c.Y] == " "&& c != landingSpot);
+        return toReturn;
+    }
+    
+}
