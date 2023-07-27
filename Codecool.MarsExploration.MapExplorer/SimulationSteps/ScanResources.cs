7e2d8eff4ec9887097ed8957ad4c7eb4fb55cec9
Surmi64 <surmi64@gmail.com>
2023-07-27 11:42:56 +0200

    [feat]<ScanResources>(ok) class functionality extended, may be ready

---
+++ Codecool.MarsExploration.MapExplorer/SimulationSteps/ScanResources.cs
@@ -0,0 +1,24 @@
+﻿using Codecool.MarsExploration.MapExplorer.Exploration;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+using Codecool.MarsExploration.MapGenerator.Calculators.Service;
+using Codecool.MarsExploration.MapGenerator.MapElements.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.SimulationSteps;
+
+public static class ScanResources
+{
+    public static Dictionary<string, HashSet<Coordinate>> ScanResource(ICoordinateCalculator coordinateCalculator,
+        Map map, Coordinate currentPosition, MarsRover.MarsRover rover)
+    {
+        var toReturn = coordinateCalculator.GetAdjacentCoordinates(currentPosition, map.Representation.GetLength(0), rover.SightDistance);
+        foreach (var c in toReturn)
+        {
+            if (rover.EncounteredResources.ContainsKey(map.Representation[c.X, c.Y]))
+            { 
+                rover.EncounteredResources[map.Representation[c.X, c.Y]].Add(c);
+            }
+        }
+
+        return rover.EncounteredResources;
+    }
+}
